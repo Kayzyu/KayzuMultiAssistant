a47384863f57b0e7778ef655f39b1e7f58ec6cff
Kayzu <96322205+Kayzyu@users.noreply.github.com>
2022-01-28 03:33:37 +0700

    Update assistant.py

--- Yukki/Decorators/assistant.py
+++ Yukki/Decorators/assistant.py
@@ -9,6 +9,7 @@ from pyrogram.types import (CallbackQuery, InlineKeyboardButton,
 from Yukki import BOT_ID, MUSIC_BOT_NAME, app, random_assistant
 from Yukki.Database import get_assistant, save_assistant
 from Yukki.Utilities.assistant import get_assistant_details
+from Yukki.Plugins.assistant import ASS_ID
 
 
 @app.on_callback_query(filters.regex("unban_assistant"))
