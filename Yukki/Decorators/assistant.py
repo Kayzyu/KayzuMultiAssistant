932b5567bc9de1aae71a8804239decb1e7a7ac2e
Kayzu <96322205+Kayzyu@users.noreply.github.com>
2022-01-28 11:10:38 +0700

    Update assistant.py

--- Yukki/Decorators/assistant.py
+++ Yukki/Decorators/assistant.py
@@ -52,23 +52,7 @@ def AssistantAdd(mystic):
             ran_ass = random.choice(random_assistant)
             assis = {
                 "saveassistant": ran_ass,
-            }
-            await save_assistant(message.chat.id, "assistant", assis)
-        ASS_ID, ASS_NAME, ASS_USERNAME, ASS_ACC = await get_assistant_details(
-            ran_ass
-        )
-            if b.status == "kicked":
-                return await message.reply_text(
-                    f"Assistant Account[{ASS_ID}] is banned.\nUnban it first to use Music Bot\n\nUsername: @{ASS_USERNAME}",
-                    reply_markup=key,
-                )
-            if b.status == "banned":
-                return await message.reply_text(
-                    f"Assistant Account[{ASS_ID}] is banned.\nUnban it first to use Music Bot\n\nUsername: @{ASS_USERNAME}",
-                    reply_markup=key,
-                )
-        except UserNotParticipant:
-            if message.chat.username:
+            )
                 try:
                     await ASS_ACC.join_chat(message.chat.username)
                 except UserAlreadyParticipant:
